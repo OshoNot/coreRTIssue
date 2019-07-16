daa6437f43a8729f529063cfbfe7429e6f16db50
filipelbc <filipelbc@brickabode.com>
2019-07-16 15:52:00 -0300

    Print values on F# version

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -8,11 +8,25 @@ let rootConnection : string =
     |> Sql.database "dbsample"
     |> Sql.str
 
+type M = {
+    Name : string;
+    Value : string
+}
+
 let ExecuteSQL (q:string) : unit =
     rootConnection
     |> Sql.connect
     |> Sql.query q
     |> Sql.executeTable
+    |> Sql.mapEachRow (function
+        | [ "name", SqlValue.String name
+            "value", SqlValue.String value ] ->
+          let user =
+            { Name = name;
+              Value = value }
+          Some user
+        | _ -> None)
+    |> List.map (printfn "%A")
     |> ignore
 
 [<EntryPoint>]
