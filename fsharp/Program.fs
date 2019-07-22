bbb704e96761ebb551b06185c0318f8793f9d9ab
Lucas Evangelista Alves Peixoto <lucaspeixoto@MacBook-Pro-de-Lucas.local>
2019-07-22 00:59:39 -0300

    Using standalone Npgsql and rd.xml file

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,36 +1,10 @@
-open Npgsql.FSharp
+open Npgsql
 
-let rootConnection : string =
-    Sql.host "database"
-    |> Sql.port 5432
-    |> Sql.username "dbuser"
-    |> Sql.password "dbpassword"
-    |> Sql.database "dbsample"
-    |> Sql.str
-
-type M = {
-    Name : string;
-    Value : string
-}
-
-let ExecuteSQL (q:string) : unit =
-    rootConnection
-    |> Sql.connect
-    |> Sql.query q
-    |> Sql.executeTable
-    |> Sql.mapEachRow (function
-        | [ "name", SqlValue.String name
-            "value", SqlValue.String value ] ->
-          let user =
-            { Name = name;
-              Value = value }
-          Some user
-        | _ -> None)
-    |> List.map (printfn "%A")
-    |> ignore
+let connectDB = 
+  new NpgsqlConnection("Host=database;Username=dbuser;Password=dbpassword;Database=dbsample")
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World!"
-    ExecuteSQL "SELECT * FROM map"
+    connectDB |> ignore
+    // ExecuteSQL "SELECT * FROM map"
     0
